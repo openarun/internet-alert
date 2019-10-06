cdf8b3b4fda8f197582be74dec8f5b1d5b856a3c
Rabin Adhikari <rabin.adk1@gmail.com>
2019-10-06 09:37:10 +0545

    Update internet_check.py
    Removed unused imports and less used variables and functions.

--- internet_check.py
+++ internet_check.py
@@ -1,20 +1,20 @@
-import requests
 import socket
 from gi.repository import Notify
 import vlc
 import time
+
 p = vlc.MediaPlayer("ambulance_alarm.mp3")
 
 
 def check_internet(host="8.8.8.8", port=53, timeout=5):
     try:
-
         socket.setdefaulttimeout(timeout)
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
         return True
-    except Exception as ex:
-        print(ex)
+    except OSError as e:
+        print(e)
         return False
+
 
 def notify_box(msg):
     # One time initialization of libnotify
@@ -25,33 +25,22 @@ def notify_box(msg):
     body = msg
     notification = Notify.Notification.new(
         summary,
-        body, # Optional
+        body  # Optional
     )
     # Actually show on screen
     notification.show()
 
-def play_siren():
-    p.play()
-
-def stop_siren():
-    p.stop()
 
 try:
-    connected = False
 
-    while(True):
+    while True:
         if check_internet():
             print("internet is aayo")
-            connected = True
             notify_box("Internet Aayo !!")
-            play_siren()
-            time.sleep(1)
+            p.play()
+            time.sleep(5)
         else:
             print("internet is down")
-            stop_siren()
+            p.stop()
 except KeyboardInterrupt:
     print("Keyboard Exiting...")
-
-
-        
- 
